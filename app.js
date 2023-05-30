0d17e8c706984771ffecca814b4e685e5bff0e87
Ian <ifd730@gmail.com>
2023-05-29 18:40:41 -0700

    First submission to final project stage 2

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const { limiter } = require("./utils/config");
+const routes = require("./routes/index");
+const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+
+mongoose.connect("mongodb://localhost:27017/wtwr_db");
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(cors());
+
+app.options("*", cors());
+
+app.use(helmet());
+
+app.use(requestLogger);
+
+app.use(limiter);
+
+/*app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});*/
+
+app.use(routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const articles = require("./articles");
+const user = require("./users");
+const NotFoundError = require("../errors/NotFoundError");
+const { login, createUser } = require("../controllers/user");
+const auth = require("../middlewares/auth");
+const {
+  validateUserInfo,
+  validateUserLogin,
+} = require("../middlewares/validator");
+
+router.use("/articles", auth, articles);
+
+router.use("/users", auth, user);
+
+router.post("/signin", validateUserLogin, login);
+
+router.post("/signup", validateUserInfo, createUser);
+
+router.use(() => {
+  throw new NotFoundError("This route does not exist");
+});
+
+module.exports = router;
